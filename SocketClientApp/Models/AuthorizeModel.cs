19a067eea81f60712295a0207c2e7fe1b7c7c504
elivaly <vetashapel@gmail.com>
2025-01-29 14:27:11 +0300

    add views

---
+++ SocketClientApp/Models/AuthorizeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SocketClientApp.Models
+{
+    public class AuthorizeModel
+    {
+        private readonly ILogger<AuthorizeModel> _logger;
+
+        public AuthorizeModel(ILogger<AuthorizeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- SocketClientApp/Program.cs
+++ SocketClientApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,13 +15,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run(builder.Configuration["ApplicationHost:Address"]);
